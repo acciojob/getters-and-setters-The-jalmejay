6af9d283726c9bd45e3d75e7fde830ed4d8234ce
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-08-14 22:36:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name=name;
-		this.age=age;
+		this._name=name;
+		this._age=age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
 	}
 	get age(){
-		return this.age;
+		return this._age;
 	}
 	set name(name){
-		this.name=name
+		this._name=name
 	}
 	set age(age){
-		this.age=age;
+		this._age=age;
 	}
 }
 
